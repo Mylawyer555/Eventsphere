9656ff1783541ecadf1e5d45a8869b03244bcb6c
unknown <samuelabel636@gmail.com>
2025-10-14 12:59:38 +1300

    update dto with class sanitizer decorators

--- src/dtos/createUser.dto.ts
+++ src/dtos/createUser.dto.ts
@@ -1,30 +1,40 @@
 import { Department, Role } from "@prisma/client";
+import { Escape, NormalizeEmail, Trim } from "class-sanitizer";
 import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Length } from "class-validator";
 
 export class CreateUserDTO {
     @IsNotEmpty({message:"firstname is required "})
-    @Length(2,50, {message:"firstname should not be less than two characters and more 50 characters"});
+    @Length(2,50, {message:"firstname should not be less than two characters and more 50 characters"})
+    @Trim()
+    @Escape()
     firstname!: string;
 
     @IsNotEmpty()
     @Length(2,50)
+    @Trim()
+    @Escape()
     lastname!: string;
 
     @IsNotEmpty()
     @IsEmail()
+    @NormalizeEmail()
+    @Trim()
     email!: string;
 
     @IsNotEmpty()
     @Length(6, 20)
+    @Trim()
     password!: string;
 
     @IsNotEmpty()
     @IsEnum(Department)
     @IsString()
+    @Trim()
     department!: Department;
 
     @IsNotEmpty()
     @IsString()
+    @Trim()
     enrollment_number!: string;
 
     @IsOptional()
--- src/dtos/login.dto.ts
+++ src/dtos/login.dto.ts
@@ -1,11 +1,14 @@
+import { Trim } from "class-sanitizer";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDTO {
     @IsNotEmpty()
     @IsEmail()
+    @Trim()
     email!: string;
 
     @IsNotEmpty()
     @IsString()
+    @Trim()
     password!: string;
 }
